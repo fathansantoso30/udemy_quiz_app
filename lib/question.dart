aca86e48e9a5ed53a47427ab15177f2381a11e06
fathansantoso30 <fathansantoso30@gmail.com>
2022-12-29 17:26:38 +0700

    added feature course content 46

---
+++ lib/question.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String questionText;
+  const Question({super.key, required this.questionText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(16),
+      width: double.infinity,
+      child: Text(
+        questionText,
+        textAlign: TextAlign.center,
+        style: const TextStyle(fontSize: 28),
+      ),
+    );
+  }
+}
